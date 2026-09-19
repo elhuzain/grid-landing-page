653eb00bde62e86158f75345e79be231af5608e1
elhuzain <a.huzain98@gmail.com>
2026-09-20 01:13:48 +0300

    feat: enhance menu button animations - Introduced a new timeline for menu button animations - Added rotation and opacity effects for open/close icons - Updated event listener to control menu button animations

--- script/index.js
+++ script/index.js
@@ -9,12 +9,17 @@ mainTl.from("h1", { duration: 0.2, opacity: 0, y: 5, ease: "power4.out" })
     .from("#hero p", { duration: 0.2, opacity: 0, y: 5, ease: "power4.out" })
     .from("article", { duration: 0.2, opacity: 0, y: 5, stagger: 0.1, ease: "power4.out" })
 
+let menuButtonsTl = gsap.timeline();
 let menuTl = gsap.timeline();
+
+menuButtonsTl.pause();
 menuTl.pause();
 
-menuTl.to("#close-icon", { duration: 0, opacity: 1 })
-    .to("#open-icon", { duration: 0, opacity: 0 })
-    .to("#nav-overlay", { duration: 0.1, opacity: 1 })
+menuButtonsTl.to("#open-icon", { duration: 0.2, opacity: 0, rotate: 90 })
+    .to("#close-icon", { duration: 0.2, opacity: 1, rotate: 90 })
+    
+
+menuTl.to("#nav-overlay", { duration: 0.1, opacity: 1 })
     .from("#nav", {
         duration: 0.2, opacity: 0, x: "100%"
     })
@@ -22,10 +27,12 @@ menuTl.to("#close-icon", { duration: 0, opacity: 1 })
 
 menuButton.addEventListener("click", () => {
     if (isMenuOpen) {
+        menuButtonsTl.reverse();
         menuTl.reverse();
         navOverlay.classList.add("pointer-events-none");
         isMenuOpen = false;
     } else {
+        menuButtonsTl.play();
         menuTl.play();
         navOverlay.classList.remove("pointer-events-none");
         isMenuOpen = true;
